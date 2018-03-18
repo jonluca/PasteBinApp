d0333af2b279c5155bab86ebe461f6680b58766c
JonLuca DeCaro <jdecaro@usc.edu>
2018-03-18 12:38:32 -0700

    full remove of dynamic blur view

--- PasteBin/Pods/Target Support Files/Pods-PasteBin/Pods-PasteBin-frameworks.sh
+++ PasteBin/Pods/Target Support Files/Pods-PasteBin/Pods-PasteBin-frameworks.sh
@@ -135,12 +135,10 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/DynamicBlurView/DynamicBlurView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Highlightr/Highlightr.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/DynamicBlurView/DynamicBlurView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Highlightr/Highlightr.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
